0f96e08c47e997ff6f4d272f2d69778041ed1986
Jason Katz <jkatz94@gmail.com>
2023-01-10 11:18:10 -0800

    Add types for transaction list

--- src/acme-api.ts
+++ src/acme-api.ts
@@ -23,6 +23,35 @@ export interface AcmeMerchantDetails {
   updated_at: string;
 }
 
+export interface AcmeMerchantTransaction {
+  id: string;
+  amount: number;
+  type: "PURCHASE" | "REFUND";
+  customer: string;
+  merchant: string;
+  order: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface MerchantTransaction {
+  id: string;
+  amount: number;
+  type: "PURCHASE" | "REFUND";
+  customer: string;
+  merchant: string;
+  order: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Page<T> {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+}
+
 export async function getMerchantDetails(
   merchantId: string
 ): Promise<MerchantDetails> {
