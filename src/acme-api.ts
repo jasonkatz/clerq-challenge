e3e9e23f9bff4697a7907bd5655f452dda260508
Jason Katz <jkatz94@gmail.com>
2023-01-10 11:31:36 -0800

    Move validation to within retry mechanism

--- src/acme-api.ts
+++ src/acme-api.ts
@@ -14,7 +14,8 @@ export async function getMerchantDetails(
   merchantId: string
 ): Promise<MerchantDetails> {
   const response = await executeApiGetRequest<AcmeMerchantDetails>(
-    `/merchants/${merchantId}`
+    `/merchants/${merchantId}`,
+    validateMerchantDetailsResponse
   );
   const { data } = response;
 
@@ -25,8 +26,15 @@ export function getMerchantTransactions(merchantId: string, upToDate: string) {
   // TODO
 }
 
+class DataValidationError extends Error {
+  constructor() {
+    super();
+  }
+}
+
 async function executeApiGetRequest<T>(
-  path: string
+  path: string,
+  validate: (data: T) => void
 ): Promise<AxiosResponse<T>> {
   // Retry 5 times, on top of the 5 axios retries that we have setup - handles parsing errors
   for (let i = 0; i < 5; ++i) {
@@ -34,29 +42,34 @@ async function executeApiGetRequest<T>(
       const response = await axios.get(`${API_URL}${path}`, {
         timeout: REQUEST_TIMEOUT_MS,
       });
+
+      validate(response.data);
+
       return response;
     } catch (e) {
-      console.log("API error", e);
+      if (e instanceof DataValidationError) {
+        console.log("Validation error - retrying");
+      } else {
+        console.log("API error - retrying");
+      }
     }
   }
 }
 
-class DataValidationError extends Error {
-  constructor() {
-    super();
-  }
-}
-
-function adaptMerchantDetailsResponse(
+function validateMerchantDetailsResponse(
   apiResponse: AcmeMerchantDetails
-): MerchantDetails {
+): void {
   if (
     new Date(apiResponse.created_at).toString() === "Invalid Date" ||
     new Date(apiResponse.updated_at).toString() === "Invalid Date"
   ) {
     throw new DataValidationError();
   }
+}
 
+function adaptMerchantDetailsResponse(
+  apiResponse: AcmeMerchantDetails
+): MerchantDetails {
   return {
     id: apiResponse.id,
     name: apiResponse.name,
