71ebab42f3a5789568923cbe6070542811fb1acf
Jason Katz <jkatz94@gmail.com>
2023-01-10 09:49:08 -0800

    Set up express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello, world!");
+import express, { Response } from "express";
+import cors from "cors";
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(cors());
+
+app.get("/test", (_, res: Response) => {
+  res.send("I am alive...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
